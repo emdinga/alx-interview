bbbf8bd591176112880c1e3deb96660b84514262
Emdinga <emdinga@gmail.com>
2024-02-29 19:43:15 +0000

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,16 +17,23 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    """ Create a list to store the fewest number
+    of coins needed for each total
+    """
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
+    """ Iterate through each coin value"""
     for coin in coins:
+        """ Update the dp list for each total amount"""
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
+    """ Return the fewest number of coins needed for the given total """
     return dp[total] if dp[total] != float('inf') else -1
 
 
 if __name__ == "__main__":
+    # Example usage
     print(makeChange([1, 2, 25], 37))
     print(makeChange([1256, 54, 48, 16, 102], 1453))
