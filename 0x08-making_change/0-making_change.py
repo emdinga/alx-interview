e243f41b08ca2eb10c89fcbfe9801223c381b5b9
Emdinga <emdinga@gmail.com>
2024-02-29 19:29:44 +0000

    0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+"""
+
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given amount total.
+    Args:
+        coins (list): A list of the values of the coins in your possession.
+        total (int): The total amount to be met.
+    Returns:
+        int: The fewest number of coins needed to meet total.
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+
+if __name__ == "__main__":
+    print(makeChange([1, 2, 25], 37))
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ ALX interview tasks
 0x05-nqueens
 0x06-starwars_api
 0x07-rotate_2d_matrix
+0x08-making_change
